590e15750d602b68a187fe1c3fb6df50c6d1bcda
irvan kurniawan <irvan.septian@photon.com>
2023-12-01 20:30:09 +0700

    condition base url

--- lib/drupal.ts
+++ lib/drupal.ts
@@ -1,7 +1,7 @@
 import { DrupalClient } from "next-drupal"
 
 export const drupal = new DrupalClient(
-  process.env.NEXT_PUBLIC_DRUPAL_BASE_URL,
+  process.env.NEXT_PUBLIC_DRUPAL_BASE_URL || '',
   {
     apiPrefix: "/api",
     previewSecret: process.env.DRUPAL_PREVIEW_SECRET,
